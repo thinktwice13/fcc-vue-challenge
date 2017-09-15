d3f575be8b96ec69bc372fcae93911eaea60d80c
thinktwice13 <marioo.krajacic@gmail.com>
2017-09-16 01:28:56 +0200

    Setup eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,39 +1,18 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  extends: 'airbnb-base',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true
+    },
+    extends: "eslint:recommended",
+    parserOptions: {
+        sourceType: "module"
+    },
+    plugins: ["html"],
+    rules: {
+        indent: ["error", 2],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "never"]
     }
-  },
-  // add your custom rules here
-  'rules': {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
 }
