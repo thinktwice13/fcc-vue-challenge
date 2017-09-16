e3dbce03f83c572c02b18dc3b8c4205c920faa5f
thinktwice13 <marioo.krajacic@gmail.com>
2017-09-16 02:10:03 +0200

    Add vuetify and home page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,60 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+  <v-app id="app" toolbar footer>
+    <v-navigation-drawer persistent v-model="drawer" light enable-resize-watcher absolute>
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar class="grey darken-3" dark fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Toolbar</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <router-view></router-view>
+        <!--v-router-->
+      </v-container>
+    </main>
+    <v-footer class="grey darken-3">
+      <span class="white--text">© 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: "app",
+  data () {
+    return {
+      drawer: false
+    }
+  },
+  mounted () {
+    this.drawer = false
+  }
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+div.container.fluid {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
