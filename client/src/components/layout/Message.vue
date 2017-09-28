bccb8da721f70dd4d180345b5222472aaa0941a7
thinktwice13 <marioo.krajacic@gmail.com>
2017-09-28 22:04:02 +0200

    Update Stock app conditional rendering

---
+++ client/src/components/layout/Message.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  v-layout(justify-center align-center)
+    div.pa-5.display-2 {{msg}}
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
+
