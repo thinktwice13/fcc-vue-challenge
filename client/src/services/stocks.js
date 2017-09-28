a85dd3c190fdb550b31e443284208cfb17152625
thinktwice13 <marioo.krajacic@gmail.com>
2017-09-28 21:04:39 +0200

    Render chart data, update data on stock add/remove

--- client/src/services/stocks.js
+++ client/src/services/stocks.js
@@ -3,5 +3,7 @@ import axios from "axios"
 const iexUrl =
   "https://api.iextrading.com/1.0/stock/market/batch?symbols=SYMBOLS&types=chart&range=1y"
 
-export const getStock = async symbol =>
-  (await axios.get(iexUrl.replace("SYMBOLS", symbol))).data
+export const getStock = async symbols => {
+  const data = (await axios.get(iexUrl.replace("SYMBOLS", symbols))).data
+  return data[symbols[0]].chart ? data : null
+}
